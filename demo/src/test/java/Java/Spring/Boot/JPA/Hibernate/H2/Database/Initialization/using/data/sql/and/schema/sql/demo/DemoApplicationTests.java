cb5d750524b248003e6c2c35e848254ba3554c7b
Jakub <jakub.bchyla@gmail.com>
2021-07-25 14:41:56 +0200

    Initial commit

---
+++ demo/src/test/java/Java/Spring/Boot/JPA/Hibernate/H2/Database/Initialization/using/data/sql/and/schema/sql/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.Spring.Boot.JPA.Hibernate.H2.Database.Initialization.using.data.sql.and.schema.sql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
